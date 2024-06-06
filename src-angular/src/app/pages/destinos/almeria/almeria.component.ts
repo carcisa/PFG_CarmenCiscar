652dca2c86da8a0c966e6d7953e54c99825dbca5
carcisa <mcisarr228@g.educaand.es>
2024-06-06 19:55:49 +0200

    Creo componente search

--- src-angular/src/app/pages/destinos/almeria/almeria.component.ts
+++ src-angular/src/app/pages/destinos/almeria/almeria.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Actividad } from '../../../models/actividad.model';
+import { DestinoService } from '../../../services/destino.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../../Components/button/button.component';
+import { SelectComponent } from '../../../Components/select/select.component';
 
 @Component({
   selector: 'app-almeria',
   standalone: true,
-  imports: [],
+  imports: [SelectComponent, FormsModule, ButtonComponent, CommonModule],
   templateUrl: './almeria.component.html',
-  styleUrl: './almeria.component.scss'
+  styleUrls: ['./almeria.component.scss']
 })
-export class AlmeriaComponent {
+export class AlmeriaComponent implements OnInit {
+  actividades: Actividad[] = [];
+  nombreDestino: string = '';
 
+  constructor(private destinoService: DestinoService) {}
+
+  ngOnInit(): void {
+    this.cargarActividades();
+  }
+
+  cargarActividades(): void {
+    const destinoId = 1;
+    this.destinoService.getDestinoById(destinoId).subscribe({
+      next: (destino) => {
+        this.nombreDestino = destino.nombre;
+        this.destinoService.getActividadesByDestinoId(destinoId).subscribe({
+          next: (data: Actividad[]) => {
+            console.log('Actividades cargadas:', data); // Verificar que se cargan las actividades
+            this.actividades = data;
+          },
+          error: (err) => {
+            console.error('Error al cargar actividades', err);
+          }
+        });
+      },
+      error: (err) => {
+        console.error('Error al cargar el destino', err);
+      }
+    });
+  }
 }
